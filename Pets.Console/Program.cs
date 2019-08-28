8c348d7d5be91548b66c14b058909e6bc3cc0a66
Leo Quenta <lquenta@gmail.com>
2019-08-28 13:01:22 -0400

    API controller and Pets console argument management

--- Pets.Console/Program.cs
+++ Pets.Console/Program.cs
@@ -3,13 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using CommandLine;
 
-namespace Pets.Console
+namespace Pets.ConsoleProgram
 {
     class Program
     {
+        public class Options
+        {
+            [Value(0, MetaName = "CSV File Path,",
+            HelpText = "CSV file to be processed,must be the file name of csv file must be relative to the executable of this console program (e.g. test.csv)",
+            Required = true)]
+            public string FileName { get; set; }
+
+            [Option('n', "name", Required = false, HelpText = "Pet Name argument to be searched.")]
+            public string NameSearch { get; set; }
+
+            [Option('t', "type", Required = false, HelpText = "Pet Type argument to be searched.")]
+            public string TypeSearch { get; set; }
+
+            [Option('g', "gender", Required = false, HelpText = "Pet Gender argument to be searched.")]
+            public string GenderSearch { get; set; }
+
+
+        }
+
         static void Main(string[] args)
         {
+           
+
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+
+                       Console.Write($"FileName:{o.FileName} ,Enabled!");
+                       if (o.NameSearch != "")
+                       {
+                           Console.WriteLine($"Name Search argument present:{o.NameSearch}");
+                       }
+                   });
+
+           /* if (args.Length == 0)
+            {
+                Console.WriteLine("Please insert a valid argument.");
+                Console.WriteLine("First argument must be the file name of csv file must be relative to the executable of this console program (e.g. test.csv)");
+                Console.WriteLine("Second argument is search type over the csv file (e.g type=cat) (e.g type=dog gender=female) ");
+
+            }
+            var filePath = args[0];
+            string filePathAbs =Path.GetFullPath(filePath);
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine(String.Format("{0} File not exists!", filePath));
+                return;
+            }
+             */
+
         }
     }
 }
---
+++ Pets.RESTService/Controllers/PetsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Pets.Logic;
+using Pets.DAO;
+using System.Configuration;
+
+namespace Pets.RESTService.Controllers
+{
+    public class PetsController : ApiController
+    {
+        // GET api/values/5
+        public ResponseSearchPet Get(string name="",string typeSearch="",string gender="")
+        {
+            ResponseSearchPet res = new ResponseSearchPet();
+            string csvFilePath= ConfigurationManager.AppSettings["CSVFilePath"].ToString();
+            Manager manager = new Manager(csvFilePath);
+            RequestSearchPet request = new RequestSearchPet();
+            if (request.SearchPetGender != "") {
+                switch (request.SearchPetGender.ToUpper())
+                {
+                    case "MALE":
+                        request.SearchPetGender="M";
+                        break;
+                    case "FEMALE":
+                        request.SearchPetGender = "F";
+                        break;
+                    default:
+                        throw new Exception("Only allowed values are MALE or FEMALE");
+                        
+                }
+            }
+            if(request.SearchPetName != "")
+            {
+                request.SearchPetName = name.Trim();
+            }
+            if(request.SearchPetType != "")
+            {
+                request.SearchPetType = typeSearch.Trim().ToUpper();
+            }
+
+            res = manager.SearchPet(request);
+
+            return res;
+        }
+
+        // POST api/values
+        public bool Post(RequestNewPet request ) //Add new Pet
+        {
+            string csvFilePath = ConfigurationManager.AppSettings["CSVFilePath"].ToString();
+            Manager manager = new Manager(csvFilePath);
+            return manager.CreatePet(request);
+             
+        }
+
+        
+
+        // DELETE api/values/5
+        public bool Delete(RequestDeletePet request)
+        {
+            string csvFilePath = ConfigurationManager.AppSettings["CSVFilePath"].ToString();
+            Manager manager = new Manager(csvFilePath);
+            return manager.DeletePet(request);
+        }
+    }
+}
